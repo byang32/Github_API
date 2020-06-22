3ce4e6e752379327c7ed80416d0d1fb076c74bb9
Bee <beeyang85@gmail.com>
2020-06-22 15:50:11 -0500

    add icon links.

--- lib/github.ex
+++ lib/github.ex
@@ -1,9 +1,9 @@
 defmodule Github do
 
-  def github_client, do: Tentacat.Client.new()
+  def github_client, do: Tentacat.Client.new(%{access_token: "f82b26dc9a070045c7c9b9d0b5c422e6145c3331"})
 
   def fetch_github(org_name) do
-    client = github_client()
+    # client = github_client()
 
     get_org(org_name) ++
     get_repos(org_name)
